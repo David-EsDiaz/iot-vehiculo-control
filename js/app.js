13a2b6ce0a9e21b2753b0ba6d03125cdb1c9daff
David-EsDiaz <71625007+David-EsDiaz@users.noreply.github.com>
2025-05-26 10:53:31 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ function sendCommand(command) {
   };
 
   // Hacemos el POST usando fetch
-  fetch("http://100.27.40.201/api/devices", {
+  fetch("http://13.220.144.76/api/devices", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
